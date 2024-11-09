041d5cd131ef3d035e2ecc15b3e52ae504265b98
nahidul-fahim <nahidul7562@gmail.com>
2024-11-09 19:57:35 +0600

    creating user module

---
+++ src/app/modules/User/user.route.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { userController } from './user.controller';
+
+const router = express.Router();
+
+router.get(
+    "/",
+    userController.createAdmin
+)
+
+
+export const userRoutes = router;
---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,13 @@
+
+
+
+const createAdmin = async () => {
+    return {
+        message: "Admin created successfully"
+    }
+}
+
+
+export const userService = {
+    createAdmin
+}
