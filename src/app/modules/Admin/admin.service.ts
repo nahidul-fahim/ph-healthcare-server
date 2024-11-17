1b00229223156b4eeb25726a925b85c1ad5e2208
nahidul-fahim <nahidul7562@gmail.com>
2024-11-17 11:02:41 +0600

    more filtering in get admin functionality

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,32 +1,36 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
-
 const prisma = new PrismaClient();
 
 const getAllAdminFromDb = async (params: any) => {
-
+    const { searchTerm, ...filterData } = params;
     const andConditions: Prisma.AdminWhereInput[] = [];
+    const searchableFields = ['name', 'email'];
 
-    if (params.searchTerm) {
+    // functionality for search term
+    if (searchTerm) {
         andConditions.push({
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
+            OR: searchableFields.map(field => ({
+                [field]: {
+                    contains: searchTerm,
+                    mode: 'insensitive'
                 }
-            ]
+            }))
         })
     }
 
-    const whereConditions: Prisma.AdminWhereInput = {AND: andConditions}
+    // functionality for filtering
+    if (Object.keys(filterData).length > 0) {
+        andConditions.push({
+            AND: Object.keys(filterData).map(key => ({
+                [key]: {
+                    equals: filterData[key]
+                }
+            }))
+        })
+    }
+
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
     const result = await prisma.admin.findMany({
         where: whereConditions
     });
