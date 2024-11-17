bf5779d102d040b83087ac7a92204a7e2c69ff78
nahidul-fahim <nahidul7562@gmail.com>
2024-11-17 11:27:55 +0600

    created pick function to filter unwanted searching fields

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
+import pick from "../../../shared/pick";
 
 const getALlAdmin = async (req: Request, res: Response) => {
-    const result = await AdminService.getAllAdminFromDb(req.query);
+    const filters = pick(req.query, ['searchTerm', 'name', 'email', 'contactNumber']);
+    const result = await AdminService.getAllAdminFromDb(filters);
     res.status(200).json({
         success: true,
         message: "Admin data fetched",
