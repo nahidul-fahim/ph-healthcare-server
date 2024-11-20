6e668458c7c2c650cb504adfae8b66bfbe7456f6
nahidul-fahim <nahidul7562@gmail.com>
2024-11-20 20:50:01 +0600

    added reusable routes component

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { userRoutes } from './app/modules/User/user.route';
 import { adminRoutes } from './app/modules/Admin/admin.route';
+import router from './app/routes';
 
 const app: Application = express();
 app.use(cors());
@@ -12,7 +13,6 @@ app.get("/", (req: Request, res: Response) => {
     res.send("PH healthcare is running!");
 })
 
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1', router);
 
 export default app;
