11201098e88d542df399f25d0bf439eaa8870448
nahidul-fahim <nahidul7562@gmail.com>
2024-12-01 11:30:21 +0600

    guard the admin routes

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -2,27 +2,33 @@ import express from "express";
 import { AdminController } from "./admin.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { adminValidationSchemas } from "./admin.validation";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 router.get(
     "/",
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.getALlAdmin
 )
 
 router.get(
     "/:id",
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.getById
 )
 
 router.patch(
     "/:id",
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     validateRequest(adminValidationSchemas.update),
     AdminController.updateData
 )
 
 router.delete(
     "/:id",
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.softDelete
 )
 
